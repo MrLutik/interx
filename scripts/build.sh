332a5d6b6bad5d021341f2eb67495aeb249c8ea0
mrlutik <yaievgeniy@gmail.com>
2024-02-27 19:24:03 +0100

    Make build static

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,6 @@ set -x
 . /etc/profile
 
 go mod tidy
-go build -ldflags "-X google.golang.org/protobuf/reflect/protoregistry.conflictPolicy=ignore" -o "${GOBIN}/interxd"
+CGO_ENABLED=0 go build -a -tags netgo -installsuffix cgo -ldflags "-X google.golang.org/protobuf/reflect/protoregistry.conflictPolicy=ignore -extldflags '-static'" -o "/interxd" .
 go mod verify
 echoInfo "INFO: Sucessfully intalled INTERX $(interxd version)"
