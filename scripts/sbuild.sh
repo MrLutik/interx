3477ac74b0b35c51cdaf359ffa7588f53bd724f6
mrlutik <yaievgeniy@gmail.com>
2024-03-06 21:35:31 +0100

    Fix sbuild

--- scripts/sbuild.sh
+++ scripts/sbuild.sh
@@ -6,4 +6,4 @@ set -x
 go mod tidy
 CGO_ENABLED=0 go build -a -tags netgo -installsuffix cgo -ldflags "-X google.golang.org/protobuf/reflect/protoregistry.conflictPolicy=ignore -extldflags '-static'" -o "/interxd" .
 go mod verify
-echoInfo "INFO: Sucessfully intalled INTERX $(interxd version)"
+echoInfo "INFO: Sucessfully intalled INTERX $(/interxd version)"
